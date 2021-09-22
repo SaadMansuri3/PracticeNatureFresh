f058c39ca6e3765e67718331348e02944f322d47
unknown <sanahaju123@gmail.com>
2021-09-22 19:49:08 +0530

    Added Edit Profile

--- NatureFreshEF/Data/Repository/RegisterRepo.cs
+++ NatureFreshEF/Data/Repository/RegisterRepo.cs
@@ -7,7 +7,7 @@ using Data.Entities;
 
 namespace Data.Repository
 {
-    public class RegisterRepo:IRegisterRepo
+    public class RegisterRepo : IRegisterRepo
     {
         private CustomerDb db;
 
@@ -15,16 +15,75 @@ namespace Data.Repository
         {
             this.db = db;
         }
-        public int AddCust(RegCustomer cust)
+        public int AddCustomer(RegCustomer cust)
         {
             db.RegCustomers.Add(cust);
             Save();
-            int id = db.RegCustomers.Local[0].id;
-            return id; 
+            return db.RegCustomers.Local[0].id;
         }
+
+        public void DeleteCustomer(int id)
+        {
+
+            var cust = db.RegCustomers.Find(id);
+            if (cust != null)
+            {
+                db.RegCustomers.Remove(cust);
+                Save();
+            }
+            else
+                throw new ArgumentException("Customer is not found");
+        }
+
+
+
+
         public void Save()
         {
             db.SaveChanges();
         }
+
+
+
+        public RegCustomer GetCustomerById(int id)
+        {
+            if (id > 0)
+            {
+                var cust = db.RegCustomers.Find(id);
+
+                if (cust != null)
+                    return cust;
+                else
+                    return null;
+            }
+            else
+            {
+                throw new ArgumentException("Id cannot be less than 0");
+            }
+
+        }
+
+
+
+        public RegCustomer UpdateCustomer(RegCustomer customer)
+        {
+            RegCustomer updatedCust = (from c in db.RegCustomers
+                                       where c.id == customer.id
+                                       select c).FirstOrDefault();
+            updatedCust.Name = customer.Name;
+            updatedCust.Username = customer.Username;
+            updatedCust.Mobile = customer.Mobile;
+            updatedCust.Address = customer.Address;
+            Save();
+
+            return customer;
+        }
+
+        public IEnumerable<RegCustomer> GetCustomers()
+        {
+            return db.RegCustomers.ToList();
+        }
+
+
     }
 }
---
+++ NatureFreshEF/NatureFreshEF/Controllers/ProfileController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NatureFreshEF.Models;
+using Data.Entities;
+using Data;
+using System.Net;
+using RegCustomer = Data.Entities.RegCustomer;
+using Data.Repository;
+
+namespace NatureFreshEF.Controllers
+{
+    public class ProfileController : Controller
+    {
+        CustomerDb db = new CustomerDb();
+        RegisterRepo repo;
+        // GET: Manage
+
+        public ProfileController()
+        {
+            repo = new RegisterRepo(new CustomerDb());
+        }
+
+
+        public ActionResult Index()
+        {
+
+            LoginViewModel objLoginModel = new LoginViewModel();
+            RegisterViewModel objRegModel = new RegisterViewModel();
+            var check = db.RegCustomers.Where(x => x.id);
+            Session["IdSS"] = objRegModel.Id;
+            var SID = Convert.ToInt32(Session["IdSS"]);
+
+            if (checkLogin != null)
+            {
+                var cust = repo.GetCustomerById(SID);
+                if (cust == null)
+                    return View(Mapper.MapCust(cust));
+                return RedirectToAction("Index");
+            }
+
+            return View(objRegModel);
+            //var cust = repo.GetCustomerById(id);*/
+            /*var cust = repo.GetCustomers();
+            var data = new List<NatureFreshEF.Models.RegisterViewModel>();
+            foreach (var c in cust)
+           {
+                data.Add(Mapper.MapCust(c));
+            }
+            return View(data);*/
+
+        }
+
+        public ActionResult GetCustomerById(int id)
+        {
+
+            var cust = repo.GetCustomerById(id);
+            if (cust == null)
+                return View(Mapper.MapCust(cust));
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        public ViewResult Create()
+        {
+
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Create(RegisterViewModel cust)
+        {
+            if (ModelState.IsValid)
+            {
+                repo.AddCustomer(Mapper.DbMapView(cust));
+                return RedirectToAction("Index");
+            }
+            return View(cust);
+        }
+
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            if (id < 1)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            var cust = repo.GetCustomerById(id);
+            if (cust == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(Mapper.MapCust(cust));
+        }
+
+
+        [HttpPost]
+        public ActionResult Edit(RegisterViewModel RegCustView, int id)
+        {
+            if (ModelState.IsValid)
+            {
+                repo.UpdateCustomer(Mapper.DbMapView(RegCustView));
+                return RedirectToAction("Index");
+            }
+            return View(RegCustView);
+        }
+
+    }
+}
+
